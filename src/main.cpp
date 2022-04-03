7f370ab34b4d06e7d54429ae4f515940e33329c9
Gbkng <guillaume.brooking@gmail.com>
2022-04-03 19:45:53 +0200

    add : header as an example

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,14 @@
-int main()
+#include <iostream>
+#include "utils.h"
+
+int main() 
 {
-    int a = 2;
-    return 0;
+  int a;
+
+  a = 2;
+  std::cout << "Hello world, a is : " << a << "\n" << std::endl;
+  int b = 3;
+  int c = mySum(a, b);  
+
+  return 0;
 }
---
+++ src/utils.cpp
@@ -0,0 +1,5 @@
+// #include "utils.h"
+
+int mySum(int a, int b) {
+    return a + b;
+}
